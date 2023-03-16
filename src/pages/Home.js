f07248b41e8912e5b73d9cbec10ddfa0c776cfd0
karthik <spoorthiachar47@gmail.com>
2023-03-16 21:04:35 +0530

    final

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -133,59 +133,59 @@ const SearchForm = () => {
         }
     };
     return (
-        <section
-            id="hero"
-            style={{
-                backgroundImage: "url('assets/generic_landing.jpg')",
-                minHeight: "500px",
-            }}
+        // <section
+        //     id="hero"
+        //     style={{
+        //         backgroundImage: "url('assets/generic_landing.jpg')",
+        //         minHeight: "500px",
+        //     }}
+        // >
+        //     <div className="hero-backdrop"></div>
+        //     <div className="container position-relative">
+        //         <div className="hero-heading mb-4">
+        //             <h1>SAVE BIG ON AIRPORT PARKING</h1>
+        //             <h2>We have the best deals for airport parking lots!</h2>
+        //         </div>
+        //         <div className="searchbox landing">
+        //             <div className="row tabs">
+        //                 <div className="tab">
+        //                     <div className="heading">Most Convenient</div>
+        //                     <div className="button">
+        //                         <div className="icon">
+        //                             <i className="fas fa-car"></i>
+        //                         </div>
+        //                         Airport Parking Only
+        //                     </div>
+        //                 </div>
+        //                 <div className="tab">
+        //                     <div className="heading">Best Value</div>
+        //                     <div className="button">
+        //                         <div className="icon">
+        //                             <i className="fas fa-bed"></i> +{" "}
+        //                             <i className="fas fa-car"></i>
+        //                         </div>
+        //                         Hotel &amp; Parking Package
+        //                     </div>
+        //                 </div>
+        //             </div>
+        <form
+            action="/results.html"
+            method="post"
+            autocomplete="off"
+            novalidate=""
         >
-            <div className="hero-backdrop"></div>
-            <div className="container position-relative">
-                <div className="hero-heading mb-4">
-                    <h1>SAVE BIG ON AIRPORT PARKING</h1>
-                    <h2>We have the best deals for airport parking lots!</h2>
-                </div>
-                <div className="searchbox landing">
-                    <div className="row tabs">
-                        <div className="tab">
-                            <div className="heading">Most Convenient</div>
-                            <div className="button">
-                                <div className="icon">
-                                    <i className="fas fa-car"></i>
-                                </div>
-                                Airport Parking Only
-                            </div>
-                        </div>
-                        <div className="tab">
-                            <div className="heading">Best Value</div>
-                            <div className="button">
-                                <div className="icon">
-                                    <i className="fas fa-bed"></i> +{" "}
-                                    <i className="fas fa-car"></i>
-                                </div>
-                                Hotel &amp; Parking Package
-                            </div>
-                        </div>
-                    </div>
-                    <form
-                        action="/results.html"
-                        method="post"
-                        autocomplete="off"
-                        novalidate=""
-                    >
-                        <div className="options row m-0">
-                            <label className="col-12 col-xl-3 p-0 mr-xl-3 mb-2">
-                                <div className="heading mb-1">Departure Airport</div>
-                                <div className="placeholder placeholder-airport">
-                                    <input
-                                        type="text"
-                                        placeholder="Departure Airport"
-                                        className="placeholder placeholder-airport"
-                                        onChange={departureAirportHandler}
-                                        value={departureAirport}
-                                    />
-                                    {/* {records.map((record, index) => {
+            <div className="options row m-0">
+                <label className="col-12 col-xl-3 p-0 mr-xl-3 mb-2">
+                    <div className="heading mb-1">Departure Airport</div>
+                    <div className="placeholder placeholder-airport">
+                        <input
+                            type="text"
+                            placeholder="Departure Airport"
+                            className="placeholder placeholder-airport"
+                            onChange={departureAirportHandler}
+                            value={departureAirport}
+                        />
+                        {/* {records.map((record, index) => {
                     const isEven = index % 2;
                     return (
                       <li
@@ -196,71 +196,71 @@ const SearchForm = () => {
                       </li>
                     );
                   })} */}
-                                    {Loading ? <h1>loading..</h1> : null}
-                                    {/* after it will go to airportsuggestion page */}
-                                    <AirportSuggetions
-                                        airports={filteredAirports}
-                                        selectAirport={selectAirport}
-                                    />
-                                    {error.departureAirport ? (
-                                        <div className="error">
-                                            <h2>Invalid Departure Airport</h2>
-                                        </div>
-                                    ) : null}
-                                </div>{" "}
-                                <i className="fas fa-map-marker-alt input-icon"></i>
-                            </label>
-                            <div className="col p-0 row m-0 mb-2 dates">
-                                <label className="col-sm-6 p-0 pr-sm-3 date_input">
-                                    <div className="heading mb-1">Parking Check-In</div>
-                                    <div className="placeholder">
-                                        <input
-                                            name="checkin"
-                                            type="date"
-                                            placeholder="Parking Check-In"
-                                            className="placeholder placeholder-airport"
-                                            style={{ width: "100%" }}
-                                            value={parkingCheckIn}
-                                            onChange={parkingCheckInHandler}
-                                        />
-                                        {error && error.parkingCheckIn ? (
-                                            <h3>Invalid checkin Date</h3>
-                                        ) : null}
-                                    </div>
-                                </label>{" "}
-                                <label className="col-sm-6 p-0 pl-sm-0 date_input">
-                                    <div className="heading mb-1">Parking Check-Out</div>
-                                    <input
-                                        name="Check-Out"
-                                        type="date"
-                                        placeholder="Parking Check-Out"
-                                        className="placeholder placeholder-airport"
-                                        style={{ width: "100%" }}
-                                        value={parkingCheckOut}
-                                        onChange={parkingCheckOutHandler}
-                                    />
-                                    {error && error.parkingCheckOut ? (
-                                        <h3>Invalid checkout Date</h3>
-                                    ) : null}
-                                </label>
+                        {Loading ? <h1>loading..</h1> : null}
+                        {/* after it will go to airportsuggestion page */}
+                        <AirportSuggetions
+                            airports={filteredAirports}
+                            selectAirport={selectAirport}
+                        />
+                        {error.departureAirport ? (
+                            <div className="error">
+                                <h2>Invalid Departure Airport</h2>
                             </div>
-                            <div className="col-12 col-xl-2 p-0 pl-xl-3 my-3 my-xl-0">
-                                <div className="d-none d-xl-block heading mb-1 invisible">
-                                    Submit
-                                </div>
-                                <button
-                                    type="submit"
-                                    className="btn btn-secondary btn-big btn-block p-2"
-                                    onClick={onSubmitHandler}
-                                >
-                                    <span>SEARCH</span>
-                                </button>
-                            </div>
+                        ) : null}
+                    </div>{" "}
+                    <i className="fas fa-map-marker-alt input-icon"></i>
+                </label>
+                <div className="col p-0 row m-0 mb-2 dates">
+                    <label className="col-sm-6 p-0 pr-sm-3 date_input">
+                        <div className="heading mb-1">Parking Check-In</div>
+                        <div className="placeholder">
+                            <input
+                                name="checkin"
+                                type="date"
+                                placeholder="Parking Check-In"
+                                className="placeholder placeholder-airport"
+                                style={{ width: "100%" }}
+                                value={parkingCheckIn}
+                                onChange={parkingCheckInHandler}
+                            />
+                            {error && error.parkingCheckIn ? (
+                                <h3>Invalid checkin Date</h3>
+                            ) : null}
                         </div>
-                    </form>
+                    </label>{" "}
+                    <label className="col-sm-6 p-0 pl-sm-0 date_input">
+                        <div className="heading mb-1">Parking Check-Out</div>
+                        <input
+                            name="Check-Out"
+                            type="date"
+                            placeholder="Parking Check-Out"
+                            className="placeholder placeholder-airport"
+                            style={{ width: "100%" }}
+                            value={parkingCheckOut}
+                            onChange={parkingCheckOutHandler}
+                        />
+                        {error && error.parkingCheckOut ? (
+                            <h3>Invalid checkout Date</h3>
+                        ) : null}
+                    </label>
+                </div>
+                <div className="col-12 col-xl-2 p-0 pl-xl-3 my-3 my-xl-0">
+                    <div className="d-none d-xl-block heading mb-1 invisible">
+                        Submit
+                    </div>
+                    <button
+                        type="submit"
+                        className="btn btn-secondary btn-big btn-block p-2"
+                        onClick={onSubmitHandler}
+                    >
+                        <span>SEARCH</span>
+                    </button>
                 </div>
             </div>
-        </section>
+        </form>
+        //         </div>
+        //     </div>
+        // </section>
     );
 };
 
@@ -281,15 +281,15 @@ function HomePage() {
                                     </div>
                                 </div>
                                 <section id="hero"
-                                // here inline css is included and double curley braces are included
+                                    // here inline css is included and double curley braces are included
                                     style={{
-                                        backgroundImage: `url(${process.env.PUBLIC_URL}/assets/generic_landing.jpg)`,
-                                        minHeight: '500px'
+                                        backgroundImage: `url(${require("./assets/generic_landing.jpg")})`,
+                                        minHeight: '500px',
                                     }}>
                                     <div className="hero-backdrop"></div>
                                     <div className="container position-relative">
                                         <div className="hero-heading mb-4">
-                                            {/* <h1>SAVE BIG ON AIRPORT PARKING</h1> */}
+                                            <h1>SAVE BIG ON AIRPORT PARKING</h1>
                                             <h2>We have the best deals for airport parking lots!</h2>
                                         </div>
                                         <div className="searchbox landing">
